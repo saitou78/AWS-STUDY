32421edb9fdc6099331a10b14aa2fb2c0434c177
saitou78 <saito.kazuma78@gmail.com>
2026-02-17 15:48:28 +0900

    EC2の追加

--- AWS-Study-Terraform/ec2.tf
+++ AWS-Study-Terraform/ec2.tf
@@ -26,3 +26,17 @@ resource "aws_instance" "aws-study-ec2" {
     Name = "aws-study-ec2"
   }
 }
+
+resource "aws_instance" "aws-study-ec2-git" {
+  ami                         = data.aws_ami.al2023.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.aws-study-subnet-1a.id
+  vpc_security_group_ids      = [aws_security_group.sg-ec2.id]
+  associate_public_ip_address = true
+  key_name                    = "saito"
+  monitoring                  = false
+
+  tags = {
+    Name = "aws-study-ec2-git"
+  }
+}
