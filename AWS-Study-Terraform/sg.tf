e5837f01cecf159773e91b5295fcd0186b5c276f
saitou78 <saito.kazuma78@gmail.com>
2026-03-06 11:36:54 +0900

    cidr_blocksの変更

--- AWS-Study-Terraform/sg.tf
+++ AWS-Study-Terraform/sg.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "sg-ec2" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["219.59.48.242/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
